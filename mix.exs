30b4c857d94fc7dfa02daa64b24cff0c7215fb17
Stephanie Wilkinson <what.happens@gmail.com>
2020-11-09 15:58:14 -0800

    rollback elixir version

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Piccadilly.MixProject do
     [
       app: :piccadilly,
       version: "0.1.0",
-      elixir: "~> 1.7",
+      elixir: "~> 1.5",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
