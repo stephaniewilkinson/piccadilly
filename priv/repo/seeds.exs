28d36e3d60fd0b1d53c65bb03a1a3652b54a4621
Shannon Skipper <shannonskipper@gmail.com>
2020-11-27 09:49:11 -0800

    Add a rudimentary seed

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,36 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+alias Piccadilly.{Repo, Accounts.User}
+alias Piccadilly.Timeline.{Group, Post}
+
+username =
+  ?a..?z
+  |> Enum.take_random(10)
+  |> List.to_string()
+
+user =
+  %User{email: "#{username}@whitehouse.gov"}
+  |> User.registration_changeset(%{password: "sekretsauceseriously42", hashed_password: "x"})
+  |> Repo.insert!()
+
+group =
+  %Group{owner: user, name: "🦹", emoji: "🧚‍♀️", description: "🙇‍♀️"}
+  |> Group.changeset()
+  |> Repo.insert!()
+
+post =
+  %Post{user: user, image_url: "Meow", likes_count: 5}
+  |> Post.changeset()
+  |> Repo.insert!()
+
+user
+|> Repo.preload(:groups)
+|> User.changeset_add_group(group)
+|> Repo.update!()
+
+post
+|> Repo.preload(:groups)
+|> Post.changeset_add_group(group)
+|> Repo.update!()
